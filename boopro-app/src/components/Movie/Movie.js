4ffdfa384b21545bf653a1f4968aab00c40e558d
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-06-24 12:26:13 +0200

    add alert box

--- boopro-app/src/components/Movie/Movie.js
+++ boopro-app/src/components/Movie/Movie.js
@@ -14,29 +14,18 @@ const Movie = ({item}) => {
 
     
 
-    const saveShow = async ()=>{
-    //   if(user?.email){
-    //     setLike(!like);
-    //     setSaved(true);
-    //     await updateDoc(movieID, {
-    //       savedShows:arrayUnion({
-    //           id:item.id,
-    //           title:item.title,
-    //           img:item.backdrop_path
-    //       })
-    //     })
-    //   }else{
-    //     alert('Please log in to save movie!')
-    //   }
-    //   console.log(saved)
-    }
+    const saveShow = () => {
+      alert(`title:${item.title},
+        tagline:${item.original_title}, 
+        overview:${item.overview}, 
+        vote_average:${item.vote_average}`);
+    };
   return (
-    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+    <div onClick={saveShow} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
     <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title}></img>
     <div className='flex flex-col justify-center absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-100 hover:bg-black/80 text-white'>
       <p className='text-3xl font-bold text-center'>{item.title}</p>
-      <p onClick={saveShow} className='absolute top-4 left-4 text-gray-300'>
-        {/* {like ? <FaHeart size='2rem'/> :<FaRegHeart size='2rem'/>} */}
+      <p className='absolute top-4 left-4 text-gray-300'>
       </p>
     </div>
   </div>
